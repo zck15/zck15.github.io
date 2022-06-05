40570e084e7371ee21c675307ea938968ed36ca1
Cankun Zhao <zhaock15@126.com>
2022-06-05 12:43:32 +0800

    gitalk_proxy.py change to UTF-8

--- _includes/comments-providers/gitalk_proxy.py
+++ _includes/comments-providers/gitalk_proxy.py
@@ -4,7 +4,6 @@ from flask_cors import CORS
 
 server = flask.Flask(__name__)
 
-# 跨域访问问题
 CORS(server, resources=r'/*')
 
 # github auth
@@ -12,7 +11,7 @@ client_id = "b5ad6687bca9e719250d"
 client_secret = "03676b4cbb88ab5b7acf245927104406e1767435"
 
 
-# 接口返回格式 {"access_token":"gho_COSr3lUITUX9b2J7krsKjNlnlNSOBw2g0oZ1","token_type":"bearer","scope":"public_repo"}
+#  {"access_token":"gho_COSr3lUITUX9b2J7krsKjNlnlNSOBw2g0oZ1","token_type":"bearer","scope":"public_repo"}
 @server.post('/get_access_token')
 def get_access_token():
     url = 'https://github.com/login/oauth/access_token'
@@ -25,8 +24,8 @@ def get_access_token():
         'accept': 'application/json'
     }
     result = requests.post(url=url, params=params, headers=headers, verify=False)
-    # 存储access_token
-    # ..暂时不需要
+    # 
+    # 
     return result.json()
 
 
